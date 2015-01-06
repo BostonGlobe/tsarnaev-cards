044802c50a38653d20c485eaf0ae0c674234e344
Gabriel Florit <gabrielflorit@gmail.com>
2015-01-06 15:44:45 -0500

    Add refine data

--- graphics/cards/js/main.js
+++ graphics/cards/js/main.js
@@ -1,3 +1,19 @@
-// Put all your code here.
-// NOTE: no need to do $(document).ready, or $(function() { etc here.
-// Just start writing js.
+var master = $('.igraphic-graphic.cards');
+
+// $('.days', master).html(_.templates.day({}));
+
+var refineData = require('./refineData.js');
+
+window.loadedTsarnaevTrial = function(json) {
+
+	var data = refineData(json);
+
+	$('pre').html(JSON.stringify(data, null, 4));
+
+};
+
+$.ajax({
+	url: 'http://www.boston.com/partners/tsarnaevtrial.jsonp',
+	dataType: 'jsonp',
+	jsonpCallback: 'loadedTsarnaevTrial'
+});
