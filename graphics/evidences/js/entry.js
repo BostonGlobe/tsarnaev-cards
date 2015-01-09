11763ba0914e9095619cd985c1882e7cdbdb26c4
Gabriel Florit <gabrielflorit@gmail.com>
2015-01-09 12:03:34 -0500

    initial evidences commit

---
+++ graphics/evidences/js/entry.js
@@ -0,0 +1,23 @@
+(function() { globe.onDefine('window.jQuery && $(".igraphic-graphic.evidences").length', function() {
+
+	require('./templates/templates.js');
+
+	var masterSelector = '.igraphic-graphic.evidences';
+	var master = $(masterSelector);
+
+	var hed = $('.hed', master);
+	if (hed.length) {
+		$('.header .main-hed').html(hed.html());
+	}
+
+	var subhed = $('.subhed', master);
+	if (subhed.length) {
+		$('.header .subhed').html(subhed.html());
+	}
+
+	$('.header').addClass('visible');
+
+
+	require('./main.js');
+
+}); }());
