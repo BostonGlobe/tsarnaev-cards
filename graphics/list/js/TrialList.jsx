09eb2bfc1032c9d83ca16835be0fcdf28166e884
Gabriel Florit <gabrielflorit@gmail.com>
2015-01-15 15:21:36 -0500

    add WitnessesOrEvidence.jsx and css

--- graphics/list/js/TrialList.jsx
+++ graphics/list/js/TrialList.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 
 var IGraphicMast = require('../../../common/js/components/IGraphicMast.jsx');
 var IGraphicHeader = require('../../../common/js/components/IGraphicHeader.jsx');
+var WitnessesOrEvidence = require('./components/WitnessesOrEvidence.jsx');
 
 module.exports = React.createClass({
 
@@ -14,6 +15,7 @@ module.exports = React.createClass({
 					hed={'Tsarnaev trial witnesses and evidence'}
 					subhed={'Lorem ipsum Exercitation ad do id id aliqua nisi esse consequat tempor mollit exercitation culpa tempor cupidatat Ut in in fugiat quis ea occaecat elit Excepteur est nulla culpa magna do eiusmod Ut. Lorem ipsum Dolore adipisicing Excepteur qui ad dolore veniam adipisicing amet cupidatat ad cupidatat velit in eu qui pariatur proident voluptate Ut in nisi.'}
 				/>
+				<WitnessesOrEvidence />
 			</div>
 		);
 	}
---
+++ graphics/list/js/components/WitnessesOrEvidence.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+module.exports = React.createClass({
+
+	render: function() {
+		return (
+			<div className='witnesses-or-evidence'>
+				witnesses-or-evidence
+			</div>
+		);
+	}
+
+});
