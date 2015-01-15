a1b1175cc7b65d395d39873676fefac6a3386950
Gabriel Florit <gabrielflorit@gmail.com>
2015-01-15 14:54:15 -0500

    see previous

---
+++ common/js/components/IGraphicHeader.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var IGraphicHeader = React.createClass({
+
+	render: function() {
+		return (
+			<div className='igraphic-header'>
+				igraphic-header
+			</div>
+		);
+	}
+
+});
+
+module.exports = IGraphicHeader;
