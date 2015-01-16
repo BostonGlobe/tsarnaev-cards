5dda940dc168f8cb565b1a535aaf8a4c936b983f
Gabriel Florit <gabrielflorit@gmail.com>
2015-01-16 14:32:43 -0500

    more

--- graphics/witnesses/js/main.js
+++ graphics/witnesses/js/main.js
@@ -1,5 +1,6 @@
 var master = $('.igraphic-graphic.witnesses');
 
+// This file will convert the raw data into something usable.
 var prepareData = require('../../../common/js/prepareData.js');
 
 window.loadedTsarnaevTrial = function(json) {
@@ -12,87 +13,94 @@ window.loadedTsarnaevTrial = function(json) {
 	var evidenceArray = _.values(data.evidences);
 	var evidenceDict = data.evidences;
 
-	var witnessCategories = _.chain(witnessesArray)
-		.pluck('category')
-		.uniq()
-		.sortBy(function(v) {
-			return v;		
-		})
-		.value();
+	function getCategories(array) {
 
-	var evidenceCategories = _.chain(evidenceArray)
-		.pluck('category')
-		.uniq()
-		.sortBy(function(v) {
-			return v;		
-		})
-		.value();
+		return _.chain(array)
+			.pluck('category')
+			.uniq()
+			.sortBy(function(v) {
+				return v;		
+			})
+			.value();
+	}
 
-	$('.witnesses-section ul.categories', master)
-		.html(_.templates.categories({
-			categories: witnessCategories
-		}))
-		.on('click', 'button', function(e) {
-			$(this).toggleClass('btn--disabled');
+	var witnessCategories = getCategories(witnessesArray);
+	var evidenceCategories = getCategories(evidenceArray);
 
-			var chosenCategories = $('.witnesses-section ul.categories li button:not(.btn--disabled)', master).map(function() {
-				var classes = $(this).attr('class').split(' ');
-				var category = classes.filter(function(klass) {
-					var match = klass.match(/^categoryName(.*)/);
-					return match;
-				})[0];
-				return category.match(/^categoryName(.*)/)[1];
-			}).get();
-
-			var	filteredWitnesses = _.filter(witnessesArray, function(witness) {
-				return _.contains(chosenCategories, witness.category);
-			});
-
-			renderWitnesses(filteredWitnesses);
-		});
-
-	$('.evidence-section ul.categories', master)
-		.html(_.templates.categories({
-			categories: evidenceCategories
-		}))
-		.on('click', 'button', function(e) {
-			$(this).toggleClass('btn--disabled');
-
-			var chosenCategories = $('.evidence-section ul.categories li button:not(.btn--disabled)', master).map(function() {
-				var classes = $(this).attr('class').split(' ');
-				var category = classes.filter(function(klass) {
-					var match = klass.match(/^categoryName(.*)/);
-					return match;
-				})[0];
-				return category.match(/^categoryName(.*)/)[1];
-			}).get();
-
-			var	filteredEvidence = _.filter(evidenceArray, function(evidence) {
-				return _.contains(chosenCategories, evidence.category);
-			});
-
-			renderEvidence(filteredEvidence);
-		});
+	var $witnesses = $('section.witnesses', master);
+	var $evidence = $('section.evidence', master);
 
 	function renderWitnesses(witnesses) {
-
-		$('.witnesses-section ul.list', master).html(_.templates.witnesses({
+		$('ul.list', $witnesses).html(_.templates.witnesses({
 			witnesses: witnesses,
 			evidences: evidenceDict,
 			categories: witnessCategories
 		}));
 	}
+
+	$('ul.categories', $witnesses)
+		.html(_.templates.categories({
+			categories: witnessCategories
+		}))
+		.on('click', 'button', function(e) {
+			$(this).toggleClass('btn--disabled');
+		});
+
+	// var	filteredWitnesses = _.filter(witnessesArray, function(witness) {
+	// 	return _.contains(chosenCategories, witness.category);
+	// });
+
 	renderWitnesses(witnessesArray);
 
-	function renderEvidence(evidence) {
+	// renderWitnesses(filteredWitnesses);
 
-		$('.evidence-section ul.list', master).html(_.templates.evidence({
-			evidences: evidence,
-			witnesses: witnessesDict,
+
+	// 		// var chosenCategories = $('section.witnesses ul.categories li button:not(.btn--disabled)', master).map(function() {
+	// 		// 	var classes = $(this).attr('class').split(' ');
+	// 		// 	var category = classes.filter(function(klass) {
+	// 		// 		var match = klass.match(/^categoryName(.*)/);
+	// 		// 		return match;
+	// 		// 	})[0];
+	// 		// 	return category.match(/^categoryName(.*)/)[1];
+	// 		// }).get();
+
+	// 	});
+
+	$('ul.categories', $evidence)
+		.html(_.templates.categories({
 			categories: evidenceCategories
-		}));
-	}
-	renderEvidence(evidenceArray);
+		}))
+		.on('click', 'button', function(e) {
+			$(this).toggleClass('btn--disabled');
+		});
+
+	// 		// var chosenCategories = $('section.evidence ul.categories li button:not(.btn--disabled)', master).map(function() {
+	// 		// 	var classes = $(this).attr('class').split(' ');
+	// 		// 	var category = classes.filter(function(klass) {
+	// 		// 		var match = klass.match(/^categoryName(.*)/);
+	// 		// 		return match;
+	// 		// 	})[0];
+	// 		// 	return category.match(/^categoryName(.*)/)[1];
+	// 		// }).get();
+
+	// 		// var	filteredEvidence = _.filter(evidenceArray, function(evidence) {
+	// 		// 	return _.contains(chosenCategories, evidence.category);
+	// 		// });
+
+	// 		renderEvidence(filteredEvidence);
+	// 	});
+
+	// renderWitnesses(witnessesArray);
+
+	// function renderEvidence(evidence) {
+
+	// 	$('section.evidence ul.list', master).html(_.templates.evidence({
+	// 		evidences: evidence,
+	// 		witnesses: witnessesDict,
+	// 		categories: evidenceCategories
+	// 	}));
+	// }
+	// renderEvidence(evidenceArray);
 
 };
 
