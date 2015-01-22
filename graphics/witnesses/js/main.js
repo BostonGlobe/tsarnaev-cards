e1c4ead7fdec95530fe1a57dfb3aa55b9f93f64f
Gabriel Florit <gabrielflorit@gmail.com>
2015-01-22 13:47:38 -0500

    refactor

--- graphics/witnesses/js/main.js
+++ graphics/witnesses/js/main.js
@@ -5,6 +5,136 @@ var Velocity = require('velocity-animate');
 // This file will convert the raw data into something usable.
 var prepareData = require('../../../common/js/prepareData.js');
 
+// function setupSection() {
+
+// }
+
+function getCategories(array) {
+
+	return _.chain(array)
+		.pluck('category')
+		.uniq()
+		.sortBy(function(v) {
+			return v;		
+		})
+		.value();
+}
+
+function filterItems(category, parent) {
+	var items = $('ul.list > li', parent);
+
+	if (category === 'All') {
+
+		// if 'All', show all items
+		items.show();
+
+	} else {
+
+		// else only show category items
+		items.hide();
+		$("ul.list li[data-category='" + category + "']", parent).show();
+	}
+}
+
+function handleCategoryClick(e) {
+
+	var ul = $(this).parents('ul');
+	var parent = ul.parents('section');
+	var buttons = $('button', ul);
+
+	// disable all the other buttons
+	buttons.addClass('btn--disabled');
+
+	// and enable this one
+	$(this).removeClass('btn--disabled');
+
+	// find this category
+	var chosenCategory = $(this).text().trim();
+
+	// and filter witnesses accordingly
+	filterItems(chosenCategory, parent);
+}
+
+function handleShayBrawnClick(e) {
+
+	var animationOptions = {
+		duration: 400,
+		easing: [0.23, 1, 0.32, 1]
+	};
+
+	var parent = $(this).parents('li');
+
+	var drawerSpacerHeight = 0;
+
+	// drawer is expanded
+	if (parent.hasClass('expanded')) {
+
+		// animate shaybrawn down
+		$('.to-down', this).get(0).beginElement();
+
+		// collapse spacer
+		Velocity($('.drawer-spacer', parent), {
+			height: 0
+		}, animationOptions);
+
+	} else {
+
+		// drawer is collapsed
+		// animate shaybrawn up
+		$('.to-up', this).get(0).beginElement();
+
+		// find the right drawer spacer height
+		drawerSpacerHeight = $('.drawer', parent).outerHeight(true);
+
+		// expand drawer spacer
+		Velocity($('.drawer-spacer', parent), {
+			height: drawerSpacerHeight
+		}, animationOptions);
+	}
+
+	parent.toggleClass('expanded');
+}
+
+function handleDrawerClick(e) {
+
+	var parent = $(this).parents('li');
+
+	// if drawer is closed, expand it
+	if (!parent.hasClass('expanded')) {
+
+		$('.shaybrawn', parent).click();
+	}
+}
+
+// if there is a hash in location, navigate to it
+function dealWithHash() {
+
+	var key;
+	var anchor;
+
+	if (location.hash) {
+
+		key = location.hash.replace('#', '');
+		anchor = getAnchorByKey(key);
+		anchor.get(0).scrollIntoView();
+		expandDrawerByAnchor(anchor);
+	}
+}
+
+function getAnchorByKey(key) {
+	return $('a[name="' + key + '"]');
+}
+
+function expandDrawerByAnchor(anchor) {
+
+	var match = anchor.parents('li');
+
+	// if this drawer is collapsed, expand it
+	if (!match.hasClass('expanded')) {
+		$('.shaybrawn', match).click();
+	}
+}
+
 window.loadedTsarnaevTrial = function(json) {
 
 	var data = prepareData(json);
@@ -15,210 +145,44 @@ window.loadedTsarnaevTrial = function(json) {
 	var evidenceArray = _.values(data.evidences);
 	var evidenceDict = data.evidences;
 
-	function getCategories(array) {
-
-		return _.chain(array)
-			.pluck('category')
-			.uniq()
-			.sortBy(function(v) {
-				return v;		
-			})
-			.value();
-	}
-
 	var witnessCategories = ['All'].concat(getCategories(witnessesArray));
 	var evidenceCategories = ['All'].concat(getCategories(evidenceArray));
 
 	var $witnesses = $('section.witnesses', master);
 	var $evidence = $('section.evidence', master);
 
-	function renderWitnesses(witnesses) {
-		$('ul.list', $witnesses).html(_.templates.witnesses({
-			witnesses: witnesses,
-			evidences: evidenceDict,
-			categories: witnessCategories
-		}));
-	}
-
-	function renderEvidence(evidence) {
-		$('ul.list', $evidence).html(_.templates.evidence({
-			evidences: evidence,
-			witnesses: witnessesDict,
-			categories: evidenceCategories
-		}));
-	}
-
-	function filterWitnesses(category) {
-
-		var items = $('ul.list > li', $witnesses);
-
-		if (category === 'All') {
-
-			// if 'All', show all items
-			items.show();
-
-		} else {
-
-			// else only show category items
-			items.hide();
-			$("ul.list li[data-category='" + category + "']", $witnesses).show();
-
-		}
-
-	}
-
-	function filterEvidence(category) {
-
-		var items = $('ul.list > li', $evidence);
-
-		if (category === 'All') {
-
-			// if 'All', show all items
-			items.show();
-
-		} else {
-
-			// else only show category items
-			items.hide();
-			$("ul.list li[data-category='" + category + "']", $evidence).show();
-
-		}
-
-	}
-
 	$('ul.categories', $witnesses)
 		.html(_.templates.categories({
 			categories: witnessCategories
 		}))
-		.on('click', 'button', function(e) {
-
-			var ul = $(this).parents('ul');
-			var buttons = $('button', ul);
-
-			// disable all the other buttons
-			buttons.addClass('btn--disabled');
-
-			// and enable this one
-			$(this).removeClass('btn--disabled');
-
-			// find this category
-			var chosenCategory = $(this).text().trim();
-
-			// and filter witnesses accordingly
-			filterWitnesses(chosenCategory);
-		});
+		.on('click', 'button', handleCategoryClick);
 
 	$('ul.categories', $evidence)
 		.html(_.templates.categories({
 			categories: evidenceCategories
 		}))
-		.on('click', 'button', function(e) {
+		.on('click', 'button', handleCategoryClick);
 
-			var ul = $(this).parents('ul');
-			var buttons = $('button', ul);
+	$witnesses.on('click', '.shaybrawn', handleShayBrawnClick);
+	$evidence.on('click', '.shaybrawn', handleShayBrawnClick);
 
-			// disable all the other buttons
-			buttons.addClass('btn--disabled');
+	$witnesses.on('click', '.wrapper', handleDrawerClick);
+	$evidence.on('click', '.wrapper', handleDrawerClick);
 
-			// and enable this one
-			$(this).removeClass('btn--disabled');
+	$('ul.list', $witnesses).html(_.templates.witnesses({
+		witnesses: witnessesArray,
+		evidences: evidenceDict,
+		categories: witnessCategories
+	}));
 
-			// find this category
-			var chosenCategory = $(this).text().trim();
-
-			// and filter evidence accordingly
-			filterEvidence(chosenCategory);
-		});
-
-	var animationOptions = {
-		duration: 400,
-		easing: [0.23, 1, 0.32, 1]
-	};
-
-	function handleDrawerClick(e) {
-
-		var parent = $(this).parents('li');
-
-		var drawerSpacerHeight = 0;
-
-		// drawer is expanded
-		if (parent.hasClass('expanded')) {
-
-			// animate shaybrawn down
-			$('.to-down', this).get(0).beginElement();
-
-			// collapse spacer
-			Velocity($('.drawer-spacer', parent), {
-				height: 0
-			}, animationOptions);
-
-		} else {
-
-			// drawer is collapsed
-			// animate shaybrawn up
-			$('.to-up', this).get(0).beginElement();
-
-			// find the right drawer spacer height
-			drawerSpacerHeight = $('.drawer', parent).outerHeight(true);
-
-			// expand drawer spacer
-			Velocity($('.drawer-spacer', parent), {
-				height: drawerSpacerHeight
-			}, animationOptions);
-		}
-
-		parent.toggleClass('expanded');
-	}
-
-	$witnesses.on('click', '.shaybrawn', handleDrawerClick);
-	$evidence.on('click', '.shaybrawn', handleDrawerClick);
-
-	$witnesses.on('click', '.wrapper', function(e) {
-
-		var parent = $(this).parents('li');
-
-		// if drawer is closed, expand it
-		if (!parent.hasClass('expanded')) {
-
-			$('.shaybrawn', parent).click();
-		}
-
-	});
-
-	renderWitnesses(witnessesArray);
-	renderEvidence(evidenceArray);
+	$('ul.list', $evidence).html(_.templates.evidence({
+		evidences: evidenceArray,
+		witnesses: witnessesDict,
+		categories: evidenceCategories
+	}));
 
 	$('ul.categories li:eq(0) button', $witnesses).click();
 	$('ul.categories li:eq(0) button', $evidence).click();
-
-	// if there is a hash in location, navigate to it
-	function dealWithHash() {
-
-		var key;
-		var anchor;
-
-		if (location.hash) {
-
-			key = location.hash.replace('#', '');
-			anchor = getAnchorByKey(key);
-			anchor.get(0).scrollIntoView();
-			expandDrawerByAnchor(anchor);
-		}
-	}
-
-	function getAnchorByKey(key) {
-		return $('a[name="' + key + '"]');
-	}
-
-	function expandDrawerByAnchor(anchor) {
-
-		var match = anchor.parents('li');
-
-		// if this drawer is collapsed, expand it
-		if (!match.hasClass('expanded')) {
-			$('.shaybrawn', match).click();
-		}
-	}
 
 	$('section', master).on('click', '.associated a', function(e) {
 
